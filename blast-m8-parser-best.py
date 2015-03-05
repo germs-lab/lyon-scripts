3cf27c3c88227747422cba7ffd15caface39d2f2
Adina Howe <adina.chuang@gmail.com>
2015-03-05 09:00:17 -0600

    fixed evalue

--- blast-m8-parser-best.py
+++ blast-m8-parser-best.py
@@ -6,9 +6,11 @@ for line in open(sys.argv[1]):
     data = line.rstrip().split('\t')
     query = data[0]
     hit = data[1]
+    evalue = float(data[-2])
 
-    if d.has_key(query):
-        continue
-    else:
-        d[query] = hit
-        print line, 
+    if evalue < 100:
+        if d.has_key(query):
+            continue
+        else:
+            d[query] = hit
+            print line, 
